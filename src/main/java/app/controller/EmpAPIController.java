eec03e0f1247f5109b887068910b661aa954cabf
bbbng <heoseonho@gmail.com>
2024-12-05 13:57:06 +0900

    delete method insert

--- src/main/java/app/controller/EmpAPIController.java
+++ src/main/java/app/controller/EmpAPIController.java
@@ -1,7 +1,11 @@
 package app.controller;
 
+import app.entity.Emp;
 import app.repository.EmpRepository;
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -10,5 +14,12 @@ public class EmpAPIController {
 
     private final EmpRepository empRepository;
 
+    
+    @DeleteMapping("/emp/{empno}")
+    public Emp deleteEmpByEmpno(@PathVariable Integer empno) {
+    	Emp emp = empRepository.findById(empno).orElseThrow();
+    	empRepository.deleteById(emp.getEmpno());
+    	return emp;
+    }
 
 }
--- src/main/java/app/repository/EmpRepository.java
+++ src/main/java/app/repository/EmpRepository.java
@@ -7,4 +7,8 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface EmpRepository extends JpaRepository<Emp, Integer> {
 
+	
+	
+	
+
 }
