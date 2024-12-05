6957a6ebd2e856b92be7a8768d45b5f8d0e45836
minjko <komj36@gmail.com>
2024-12-05 11:39:10 +0900

    init project

---
+++ src/main/java/app/Lion21205Application.java
@@ -0,0 +1,13 @@
+package app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lion21205Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lion21205Application.class, args);
+	}
+
+}
